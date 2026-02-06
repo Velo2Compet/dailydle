edcee01627a21218f511de276a6315b9261b4696
Velo2Compet <velo2compet@gmail.com>
2026-02-06 11:48:59 +0100

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { fetchCategories, filterRegisteredCollections } from "@/lib/quizzdle-api";
 import { HomeView } from "@/components/HomeView";
 
+// Force dynamic rendering (blockchain RPC calls can't run at build time)
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const allCategories = await fetchCategories();
   // Only show collections that are registered in the smart contract
