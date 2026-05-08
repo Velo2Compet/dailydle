9e9f7ae49a50aae0cad9f150d3847c865eb4b1f8
Velo2Compet <velo2compet@gmail.com>
2026-05-08 13:52:51 +0200

    update collaborative because it ain't really related to the team but its a collab

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     <footer className="w-full px-2 sm:px-4 py-2 sm:py-4">
       <div className="max-w-[1200px] mx-auto p-2 sm:p-3 md:p-4 rounded-xl bg-white/[0.08] backdrop-blur-lg border border-white/10">
         <p className="text-xs text-white/60 text-center">
-          &copy; {currentYear} - made with love by the{" "}
+          &copy; {currentYear} - made with love in collaboration with the{" "}
           <a
             href="https://quizzdle.com"
             target="_blank"
